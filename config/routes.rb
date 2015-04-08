0a272638eaf749d0e81b7efacef52d808bab70b1
ben lyman <bnlyman@gmail.com>
2015-04-08 14:43:53 -0600

    generated users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
+  get 'users/show'
+
+  get 'users/index'
+
   devise_for :users
   root to: "home#index"
 end
