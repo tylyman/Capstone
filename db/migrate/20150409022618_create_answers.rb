fa98d85a38080662695ec009c8942f9a9552d009
Jake Oldhamhu33er <jakemoldham@gmail.com>
2015-04-08 20:32:27 -0600

    generated the answer model and controller

---
+++ db/migrate/20150409022618_create_answers.rb
@@ -0,0 +1,12 @@
+class CreateAnswers < ActiveRecord::Migration
+  def change
+    create_table :answers do |t|
+      t.text :content
+      t.integer :question_id
+      t.integer :user_id
+      t.integer :helpful
+
+      t.timestamps null: false
+    end
+  end
+end
