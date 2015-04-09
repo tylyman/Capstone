bd7ceef6d1d8e34126598fdc8552619316201166
Jake Oldhamhu33er <jakemoldham@gmail.com>
2015-04-09 10:19:42 -0600

    added associations to Users Events answers and questions

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150408212331) do
+ActiveRecord::Schema.define(version: 20150409022618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "question_id"
+    t.integer  "user_id"
+    t.integer  "helpful"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
