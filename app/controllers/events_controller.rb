199568714af5c091d3d3034882c0a40beb944f4f
Jake Oldhamhu33er <jakemoldham@gmail.com>
2015-04-12 17:58:41 -0600

    worked on events controller and forms/views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,22 +1,53 @@
 class EventsController < ApplicationController
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      redirect_to :new
+    end
   end
 
   def edit
+    @event = Event.find(params[:id])
+    if @event.user != current_user
+      redirect_to @event, notice: "You are not authorized to edit this event."
+    end
   end
 
   def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @event = Event.find(params[:id])
+    if @event.user != current_user
+      redirect_to @event, notice: "You are not authorized to delete this event."
+    else
+    @event.destroy
+    redirect_to users_show_path
+    end
   end
 
   def show
+    @event = Event.find(params[:id]) 
   end
 
   def index
+    @events = Event.all
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:title, :description, :datetime, :city, :state, :cost, :vacancies, :created_at, :updated_at)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150409022618) do
+ActiveRecord::Schema.define(version: 20150412235328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20150409022618) do
     t.integer  "vacancies"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "questions", force: :cascade do |t|
