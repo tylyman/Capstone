af8d1839f4a9fe85a64f3fdd37371f365417f240
ben lyman <bnlyman@gmail.com>
2015-04-13 14:30:19 -0600

    Added devise test helper

--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -1,13 +1,18 @@
 require 'test_helper'
 
 class QuestionsControllerTest < ActionController::TestCase
+  def setup
+    @user = users(:one)
+    @question = questions(:one)
+    sign_in @user
+  end
   test "should get index" do
     get :index
     assert_response :success
   end
 
   test "should get show" do
-    get :show
+    get :show, id: @question.id
     assert_response :success
   end
 
