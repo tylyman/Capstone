4e80a1669b1df00cb5a5fbc8b89c40d121fcdada
ben lyman <bnlyman@gmail.com>
2015-04-16 17:18:23 -0600

    removed unneccesary methods in helpers

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,9 +1,2 @@
 module AnswersHelper
-	def check_for_language(var)
-    Obscenity.profane?(var.content)
-  end
-
-  def check_title_for_language_obscene(var)
-    Obscenity.profane?(var.title)
-  end
 end
