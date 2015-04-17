f38f4804a06216a07a20e441f22026f3c3a888d5
Jake Oldhamhu33er <jakemoldham@gmail.com>
2015-04-17 17:07:04 -0600

    made the user not be able to enroll into events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  has_and_belongs_to_many :users
+  has_and_belongs_to_many :users, -> { uniq }
 
   def spots_left
     self.total_spots - self.users.count
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_and_belongs_to_many :events
+  has_and_belongs_to_many :events, -> { uniq }
   has_many :questions
 
   # Include default devise modules. Others available are:
