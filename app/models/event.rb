9fed85673c1d6b399add2fea54709036aab35ab3
Jake Oldhamhu33er <jakemoldham@gmail.com>
2015-04-17 16:36:28 -0600

    added an enroll enviornment and alows users to enroll in events.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,7 @@
 class Event < ActiveRecord::Base
   has_and_belongs_to_many :users
 
-  def vaca_countdown
-    self.votes -= 1
-    self.save
+  def spots_left
+    self.total_spots - self.users.count
   end
 end
