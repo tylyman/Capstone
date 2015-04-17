2fb90f0195b98a46be86f6dcaffde723c2f69ca3
Jake Oldhamhu33er <jakemoldham@gmail.com>
2015-04-17 10:21:51 -0600

    fixed logo isues and also added total_spots migration and added drop down to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ActiveRecord::Base
   has_and_belongs_to_many :users
+
+  def vaca_countdown
+    self.votes -= 1
+    self.save
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413225015) do
+ActiveRecord::Schema.define(version: 20150417060406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150413225015) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.integer  "total_spots"
   end
 
   create_table "events_users", id: false, force: :cascade do |t|
