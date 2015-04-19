f6223c370670a8d7b1844fcd137c6acac48baac8
ben lyman <bnlyman@gmail.com>
2015-04-19 13:38:08 -0600

    Added vote_badge methode in answers helper and vote.js.erb

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,2 +1,9 @@
 module AnswersHelper
+  def vote_badge(link)
+    if answer.helpful >= 0
+      content_tag :span, answer.helpful, class: ['label','label-info'] 
+    else  
+      content_tag :span, answer.helpful, class: ['label','label-warning'] 
+    end  
+  end
 end
