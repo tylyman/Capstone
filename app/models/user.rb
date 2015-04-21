25aad50ebedf94fce95c43287f5080969d0099c8
Tyler Lyman <tylyman@gmail.com>
2015-04-21 13:49:59 -0600

    Added new_with_session to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,13 @@ class User < ActiveRecord::Base
     	user.image = auth.info.image # assuming the user model has an image
   	end
 	end
+
+	#copies the facebook email if available
+	def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
 end
