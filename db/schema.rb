12fd34a22a4e4195c9792f82651168f8a8fc6faf
Tyler Lyman <tylyman@gmail.com>
2015-04-21 13:09:42 -0600

    Generated column for provider/uid

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150419201045) do
+ActiveRecord::Schema.define(version: 20150421190830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,8 @@ ActiveRecord::Schema.define(version: 20150419201045) do
     t.string   "user_name"
     t.datetime "dob"
     t.boolean  "admin",                  default: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
