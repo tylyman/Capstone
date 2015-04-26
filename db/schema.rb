a09c50bf05f736ea6d16813c84660e3fd534d2f5
ben lyman <bnlyman@gmail.com>
2015-04-26 17:56:47 -0600

    Migrated new  columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150424181915) do
+ActiveRecord::Schema.define(version: 20150426235543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,9 @@ ActiveRecord::Schema.define(version: 20150424181915) do
     t.integer  "user_id"
     t.integer  "total_spots"
     t.integer  "events_owner_id"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.string   "address"
   end
 
   create_table "events_users", id: false, force: :cascade do |t|
