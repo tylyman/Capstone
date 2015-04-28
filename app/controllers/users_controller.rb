95af361a9da5b5a7a7e3e20d6e47af6dbfa440fa
ben lyman <bnlyman@gmail.com>
2015-04-28 10:18:44 -0600

    Added admin view and action with new route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  
+
   def show
     @user = User.find(params[:id])
     @transactions = @user.transactions
+  end
+
+  def admin
+  	@user = User.find(params[:id])
+  	@users = User.all
+
+  	if !current_user.admin?
+			flash[:danger] = "You are not authorized to view this page."
+ 	  	redirect_to root_url
+		end
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
   
   resources :users, only: [:index, :show]
 
+  get '/admin', to: 'users#admin', as: :admin
+
   resources :questions do
     resources :answers #only: [:index, :show, :new, :create, :edit, :update, :destroy]
   end
