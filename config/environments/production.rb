a9624ab65440bf27cbfe02ff926744d7285d2258
Jake Oldhamhu33er <jakemoldham@gmail.com>
2015-04-29 21:54:10 -0600

    added paperclip s3 url to the production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,8 @@ Rails.application.configure do
     :bucket => ENV['agogelabs'],
     :access_key_id => ENV['AKIAIPFK4STYKRUW7BQQ'],
     :secret_access_key => ENV['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']
+     Paperclip::Attachment.default_options[:url] = ':agogelabs.s3.amazonaws.com'
+     Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
     }
   }
 
