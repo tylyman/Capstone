a36b4d8adc2524d0ec31a824d071e867e639e36f
ben lyman <bnlyman@gmail.com>
2015-05-03 10:34:34 -0600

    Added route for delete user

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
   
   resources :users, only: [:index, :show]
 
+  match 'users/:id' => 'users#destroy', :via => :delete, :as => :admin_destroy_user
+
   get '/edit_admin/:id', to: 'users#edit_admin', as: :edit_admin
 
   patch '/upd_admin/:id', to: 'users#upd_admin', as: :upd_admin
