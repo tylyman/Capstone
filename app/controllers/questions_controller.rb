bdec000a764ffe3206722e98dbad33ec8559fdb5
ben lyman <bnlyman@gmail.com>
2015-05-05 12:15:01 -0600

    Removed unnecessary passing of local variable to display_users_questions.html.erb

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
-  before_action :set_user
+
   before_action :authenticate_user!, except: [:show, :index]
   
   def show
